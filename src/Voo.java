7e6e1fa9c10960e6467aea5c12f82e5012b3c701
Rener <renerboy_@hotmail.com>
2020-12-21 07:52:45 -0300

    conclusão da base da classe voo

---
+++ src/Voo.java
@@ -0,0 +1,13 @@
+public class Voo {
+    private String status;
+    private int id;
+    private Localizacao origem;
+    private Localizacao destino;
+    private Aviao aviao;
+    private Data dataSaida;
+    private Data dataChegada;
+    private int vagas;
+
+    public void adiarVoo(){}
+    public void cancelarVoo(){}
+}
